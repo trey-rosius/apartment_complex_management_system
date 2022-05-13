ccc8624c435a79d3120a24d4c5fe9cf386c8fe0e
trey-rosius <treyrosius@gmail.com>
2022-05-13 03:47:31 +0100

    Initial commit

---
+++ test/acms.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Acms from '../lib/acms-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/acms-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Acms.AcmsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
